83bc6bad542755eeaf52ab1d27dd70969ab236f9
finoriko <finoriko@Hotmail.com>
2020-03-24 22:00:55 +0900

    Add Camera and Effect

--- InfiniminerMono/Game1.cs
+++ InfiniminerMono/Game1.cs
@@ -40,6 +40,7 @@ namespace Infiniminer
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+        
             // TODO: use this.Content to load your game content here
         }
 
